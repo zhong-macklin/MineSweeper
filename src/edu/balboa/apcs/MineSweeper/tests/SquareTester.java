27a3e45b8d447cfd61a8d0e3df478e469823880f
zhong-macklin <macklinz123@gmail.com>
2016-03-08 15:33:21 -0800

    Update SquareTester.java

--- src/edu/balboa/apcs/MineSweeper/tests/SquareTester.java
+++ src/edu/balboa/apcs/MineSweeper/tests/SquareTester.java
@@ -1,1 +1,106 @@
 
+
+package edu.balboa.apcs.MineSweeper.tests;
+import edu.balboa.apcs.MineSweeper.*;
+/**
+ * This class tests the Square class' methods.
+ * 1
+ * @author mferraro
+ */
+public class SquareTester {
+
+	public static void runTests(MineSweeperTester t) {
+
+		String desc; //description of current test
+
+		///////////////////////////////////////////////////////////////////////
+		// TEST 1:  default square should not have a mine
+		///////////////////////////////////////////////////////////////////////
+		desc = "default square has no mine";
+		try {
+			Square s1 = new Square();
+			if ( s1.getIsRevealed() == false ) {
+				t.pass( desc );
+			} else {	
+				t.fail( desc );
+			}
+		} catch ( Exception e ) { //some failure occurred
+			t.fail(desc + " -- " + e);
+		}
+		// END TEST 1 
+		
+		
+		desc = "default square is not flagged";
+		try {
+			Square s1 = new Square();
+			if ( s1.getIsFlagged() == false ) {
+				t.pass( desc );
+			} else {	
+				t.fail( desc );
+			}
+		} catch ( Exception e ) { //some failure occurred
+			t.fail(desc + " -- " + e);
+		}
+		// END TEST 2
+		
+
+		desc = "default square is not mined";
+		try {
+			Square s1 = new Square();
+			if ( s1.getIsMined() == false ) {
+				t.pass( desc );
+			} else {	
+				t.fail( desc );
+			}
+		} catch ( Exception e ) { //some failure occurred
+			t.fail(desc + " -- " + e);
+		}
+		// END TEST 3 
+		
+		
+		desc = "state changes if square is revealed";
+		try {
+			Square s1 = new Square();
+			s1.setIsRevealed(true);
+			if ( s1.getIsRevealed() ) {
+				t.pass( desc );
+			} else {	
+				t.fail( desc );
+			}
+		} catch ( Exception e ) { //some failure occurred
+			t.fail(desc + " -- " + e);
+		}
+		// END TEST 4
+		
+
+		desc = "state changes if square is flagged";
+		try {
+			Square s1 = new Square();
+			s1.setIsFlagged(true);
+			if ( s1.getIsFlagged() ) {
+				t.pass( desc );
+			} else {	
+				t.fail( desc );
+			}
+		} catch ( Exception e ) { //some failure occurred
+			t.fail(desc + " -- " + e);
+		}
+		// END TEST 5
+		
+		
+		desc = "state changes if square is mined";
+		try {
+			Square s1 = new Square();
+			s1.setIsMined(true);
+			if ( s1.getIsMined() ) {
+				t.pass( desc );
+			} else {	
+				t.fail( desc );
+			}
+		} catch ( Exception e ) { //some failure occurred
+			t.fail(desc + " -- " + e);
+		}
+		// END TEST 6
+	}
+
+}
