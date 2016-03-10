05564c9cf0208ee40492e900e435632d4dd6c75e
rejano-rassel <rasselrejano18@gmail.com>
2016-03-10 14:54:06 -0800

    Initial commit

--- src/edu/balboa/apcs/MineSweeper/Scoreboard.java
+++ src/edu/balboa/apcs/MineSweeper/Scoreboard.java
@@ -1,13 +1,7 @@
 package edu.balboa.apcs.MineSweeper;
 
 public class Scoreboard {
-	/*booleans # of wins & losses
-	 * methodsadd win, add loss
-	 * -control class :need variable to know all squars that have 
-	 * no mines(if all clicked = win)
-	 * square has mine & clicked = loss
-	 * until game ends/quit
-	 */
+	
 	
 	private int wins = 0;
 	private int losses = 0;
