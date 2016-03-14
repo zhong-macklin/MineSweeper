e0530606fda2a2af3d238110d6cb8790cd648c38
vivianeng <vivian_eng@yahoo.com>
2016-03-13 21:57:08 -0700

    added javadoc comments.

--- src/edu/balboa/apcs/MineSweeper/MineSweeper.java
+++ src/edu/balboa/apcs/MineSweeper/MineSweeper.java
@@ -1,28 +1,37 @@
 package edu.balboa.apcs.MineSweeper;
 
+//import MineSweeper;
+
 import java.util.Scanner;
 
 /**
- * This class is the driver for the APCS MineSweeper app.
+ * Tcounts class is the driver for the APCS MineSweeper app.
  * 
- * @author team X
+ * @author rassel, vivian  team P
  */
 
 public class MineSweeper {
 
-	private static MineField m1;
-	private char type;
-	private int x;
-	private int y;
-	private static MakeBoard print;
-	private static Scoreboard s = new Scoreboard();
-	private static boolean win = false;
-	private static boolean lose = false;
+	static Square[][] count; // keep in mind that we have (2) 2D arrays - count
+								// & board. we need to fix this.
+	static MineField m1;
+	static int winCount;
+	char type;
+	int x;
+	int y;
+	static MakeBoard print;
+	static Scoreboard s = new Scoreboard();
+	static boolean win = false;
+	static boolean lose = false;
 
+	
+	/**
+	 * This method starts the game. Prompts the user to choose a specific level.
+	 */
 	public void beginGame() {
 
 		for (int i = 0; i < 1;) {
-			System.out.println("Please choose a difficulty:\n(b for beginner, i for intermediate, or a for advanced)");
+			System.out.println("Please choose a difficulty: b for beginner, i for intermediate, or a for advanced ");
 			Scanner abc = new Scanner(System.in);
 			String n = abc.nextLine();
 			n = n.toLowerCase().trim();
@@ -49,15 +58,22 @@ public class MineSweeper {
 		}
 	}
 
+	
+	/**
+	 * This method prompts the user to start playing the game. Prompts the user to inspect,
+	 * flag, unflag or quit the game. This method checks what the user inputs and sees
+	 * if the player wins or loses.
+	 * 
+	 * @param m board
+	 */
 	public void playGame(MineField m) {
 		win = false;
 		lose = false;
-		MakeBoard.grid(m1);
-		
+
 		for (int o = 0; o < 1;) {
-			
+			MakeBoard.grid(m1);
 			System.out.println(
-					"\nType in a command:\n(i to inspect, f to flag, u to unflag, or q to quit this game, and then coordinates)\nExample: i b3");
+					"\nType in a command: i to inspect, f to flag, u to unflag, or q to quit this game, and then coordinates.\nExample: i b3");
 			Scanner keyboard2 = new Scanner(System.in);
 			String command = keyboard2.nextLine();
 			command.trim();
@@ -65,7 +81,6 @@ public class MineSweeper {
 				return;
 			}
 			parseCommand(command);
-			MakeBoard.grid(m1);
 
 			if (win) {
 				System.out.println("You have won the game!");
@@ -95,6 +110,11 @@ public class MineSweeper {
 
 	}
 
+	/**
+	 * This method takes in what the user inputs(it's command) and deciphers it. 
+	 * 
+	 * @param command
+	 */
 	public void parseCommand(String command) {
 		type = '0';
 		x = -1;
@@ -109,7 +129,7 @@ public class MineSweeper {
 				// System.out.println("type =" + ch);
 			} else if (x == -1) {
 				if (ch < 'a' || ch >= 'z') {
-					System.out.println("Invalid, please try again.");
+					System.out.println("Invalid, please try again. 1");
 					return;
 				} else {
 					x = command.toLowerCase().charAt(i) - 'a';
@@ -117,7 +137,7 @@ public class MineSweeper {
 				}
 			} else if (y == -1) {
 				if (ch <= '0' || ch > '9') {
-					System.out.println("Invalid, please try again.");
+					System.out.println("Invalid, please try again. 2");
 					return;
 				} else {
 					y = Character.getNumericValue(ch);
@@ -125,14 +145,14 @@ public class MineSweeper {
 				}
 			} else if (y < 10 && y > -1) {
 				if (ch < '0' || ch > '9') {
-					System.out.println("Invalid, please try again.");
+					System.out.println("Invalid, please try again. 3");
 					return;
 				} else {
 					y = 10 * y + Character.getNumericValue(ch);
 					 System.out.println("y = " + y);
 				}
 			} else {
-				System.out.println("Invalid, please try again.");
+				System.out.println("Invalid, please try again. 4");
 				return;
 			}
 
@@ -146,7 +166,7 @@ public class MineSweeper {
 				curSquare.setIsFlagged(true);
 
 			} catch (Exception e) {
-				System.out.println("Invalid, please try again.");
+				System.out.println("Invalid, please try again. 5");
 				return;
 			}
 		} else if (type == 'u') {
@@ -154,7 +174,7 @@ public class MineSweeper {
 				Square curSquare = m1.getSquareAt(y, x);
 				curSquare.setIsFlagged(false);
 			} catch (Exception e) {
-				System.out.println("Invalid, please try again.");
+				System.out.println("Invalid, please try again. 6");
 				return;
 			}
 		} else if (type == 'i') {
@@ -203,9 +223,15 @@ public class MineSweeper {
 
 	}
 
+	
+	/**
+	 * This is where the game calls upon the beginGame method and playGame method.
+	 * Asks the user if they would like to play again or quit after one game.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		MineSweeper s1 = new MineSweeper();
-		System.out.println("Welcome to APCS MineSweeper.\n");
+		System.out.println("Welcome to APCS MineSweeper.");
 		s1.beginGame();
 		// System.out.println("begin game done");
 		s1.playGame(m1);
@@ -223,7 +249,7 @@ public class MineSweeper {
 			} else {
 				System.out.println("Invalid command");
 			}
-			// uncomment if your team decides to use the provided
+			// uncomment if your team decides to ue the provided
 			// console-like UI class instead of running MineSweeper
 			// at the command line (in a terminal shell):
 			//
